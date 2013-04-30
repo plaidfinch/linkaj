4a60c356263fdd3eeb068f29a8f85ef8b8e514d3
Kenneth Foner <kenny.foner@gmail.com>
2013-04-30 13:23:33 -0400

    Lots of code cleanup.

--- src/map_experiments/core.clj
+++ src/map_experiments/core.clj
@@ -41,8 +41,7 @@
 (deftype- SetMap [metadata contents]
   IPersistentMap
     (assoc [this k v]
-           (SetMap. metadata
-                    (assoc contents k ((fnil conj #{}) (get contents k) v))))
+           (SetMap. metadata (assoc contents k ((fnil conj #{}) (get contents k) v))))
     (without [this k]
              (SetMap. metadata (dissoc contents k)))
   Associative
@@ -62,12 +61,12 @@
   MapEquivalence
   IPersistentSet
     (disjoin [this [k v]]
-             (if (not (contains? contents k))
-                 this
+             (if (contains? contents k)
                  (SetMap. metadata
                           (if (< 1 (count (get contents k)))
                               (assoc contents k (disj (get contents k) v))
-                              (dissoc contents k)))))
+                              (dissoc contents k)))
+                 this))
   IFn   (invoke [this k] (get contents k))
   IObj  (withMeta [this new-meta] (SetMap. new-meta contents))
   IMeta (meta [this] metadata))
@@ -83,68 +82,66 @@
                        (assoc (dissoc active (get mirror v)) k v)
                        (assoc (dissoc mirror (get active k)) v k)))
     (without [this k] (disj this [k (get active k)]))
-  Associative
-    (containsKey [this k] (contains? active k))
-    (entryAt     [this k] (find active k))
   IPersistentCollection
-    (count [this]       (count active))
     (cons  [this [k v]] (assoc this k v))
-    (empty [this]       (Bijection. metadata (empty active) (empty mirror)))
-    (equiv [this o]     (or (and (isa? (class o) Bijection)
-                                 (= active (.active ^Bijection o)))
-                            (= active o)))
-  Seqable (seq [this] (seq active))
-  ILookup
-    (valAt [this k]           (get active k))
-    (valAt [this k not-found] (get active k not-found))
-  MapEquivalence
+    (equiv [this o] 
+           (or (and (isa? (class o) Bijection)
+                    (= active (.active ^Bijection o)))
+               (= active o)))
+    (count [this] (count active))
+    (empty [this] (Bijection. metadata (empty active) (empty mirror)))
   IPersistentSet
     (disjoin [this [k v]]
              (if (and (contains? active k) (contains? mirror v))
                  (Bijection. metadata (dissoc active k) (dissoc mirror v))
                  this))
-  IFn   (invoke [this k] (get active k))
-  IObj  (withMeta [this new-meta] (Bijection. new-meta active mirror))
-  IMeta (meta [this] metadata))
+  IObj (withMeta [this new-meta] (Bijection. new-meta active mirror))
+  ; Everything below here is the same for all mirrored maps.
+  Associative
+    (containsKey [this k] (contains? active k))
+    (entryAt     [this k] (find active k))
+  ILookup
+    (valAt [this k]           (get active k))
+    (valAt [this k not-found] (get active k not-found))
+  Seqable (seq    [this]   (seq active))
+  IFn     (invoke [this k] (get active k))
+  IMeta   (meta   [this]   metadata)
+  MapEquivalence)
 
-; Dual (invertible) SetMap with no restrictions on associations.
+; Dual (invertible) SetMap with no restrictions on associations; that is to say, a bipartite graph.
 (deftype- Bipartite [metadata
                     ^SetMap active
                     ^SetMap mirror]
   Invertible (inverse [this] (Bipartite. metadata mirror active))
   IPersistentMap
-    (assoc [this k v]
-           (conj this [k v]))
-    (without [this k]
-             (reduce disj this (map (partial vector k) (get active k))))
+    (assoc   [this k v] (conj this [k v]))
+    (without [this k]   (reduce disj this (map (partial vector k) (get active k))))
+  IPersistentCollection
+    (cons [this [k v]]
+          (Bipartite. metadata (assoc active k v) (assoc mirror v k)))
+    (equiv [this o]
+           (or (and (isa? (class o) Bipartite)
+                    (= active (.active ^Bipartite o)))
+               (= active o)))
+    (count [this] (count active))
+    (empty [this] (Bipartite. metadata (empty active) (empty mirror)))
+  IPersistentSet
+    (disjoin [this [k v]]
+             (if (and (contains? active k) (contains? mirror v))
+                 (Bipartite. metadata (disj active [k v]) (disj mirror [v k]))
+                 this))
+  IObj (withMeta [this new-meta] (Bipartite. new-meta active mirror))
+  ; Everything below here is the same for all mirrored maps.
   Associative
     (containsKey [this k] (contains? active k))
     (entryAt     [this k] (find active k))
-  IPersistentCollection
-    (count [this] (count active))
-    (cons [this [k v]]
-          (Bipartite. metadata
-                      (assoc active k v)
-                      (assoc mirror v k)))
-    (empty [this] (Bipartite. metadata (empty active) (empty mirror)))
-    (equiv [this o] (or (and (isa? (class o) Bipartite)
-                             (= active (.active ^Bipartite o)))
-                        (= active o)))
-  Seqable (seq [this] (seq active))
   ILookup
     (valAt [this k]           (get active k))
     (valAt [this k not-found] (get active k not-found))
-  MapEquivalence
-  IPersistentSet
-    (disjoin [this [k v]]
-             (if (not (and (contains? active k) (contains? mirror v)))
-                 this
-                 (Bipartite. metadata
-                             (disj active [k v])
-                             (disj mirror [v k]))))
-  IFn   (invoke [this k] (get active k))
-  IObj  (withMeta [this new-meta] (Bipartite. new-meta active mirror))
-  IMeta (meta [this] metadata))
+  Seqable (seq    [this]   (seq active))
+  IFn     (invoke [this k] (get active k))
+  IMeta   (meta   [this]   metadata)
+  MapEquivalence)
 
 ; Drop-in replacement for normal associative map, with the additional functionality of invertibility. Yields an InvertedSurjection when inverted.
 (deftype- Surjection [metadata
@@ -152,38 +149,36 @@
                      ^SetMap mirror]
   Invertible (inverse [this] (inverted-surjection- metadata mirror active))
   IPersistentMap
-    (assoc [this k v]
-           (conj this [k v]))
-    (without [this k]
-             (disj this [k (get active k)]))
+    (assoc   [this k v] (conj this [k v]))
+    (without [this k]   (disj this [k (get active k)]))
+  IPersistentCollection
+    (cons [this [k v]]
+          (Surjection. metadata
+                       (assoc active k v)
+                       (assoc (disj mirror [(get active k) k]) v k)))
+    (equiv [this o]
+           (or (and (isa? (class o) Surjection)
+                    (= active (.active ^Surjection o)))
+               (= active o)))
+    (count [this] (count active))
+    (empty [this] (Surjection. metadata (empty active) (empty mirror)))
+  IPersistentSet
+    (disjoin [this [k v]]
+             (if (and (contains? active k) (contains? mirror v))
+                 (Surjection. metadata (dissoc active k) (disj mirror [v k]))
+                 this))
+  IObj (withMeta [this new-meta] (Surjection. new-meta active mirror))
+  ; Everything below here is the same for all mirrored maps.
   Associative
     (containsKey [this k] (contains? active k))
     (entryAt     [this k] (find active k))
-  IPersistentCollection
-    (count [this] (count active))
-    (cons [this [k v]]
-          (Surjection. metadata
-                      (assoc active k v)
-                      (assoc (dissoc mirror (get active k)) v k)))
-    (empty [this] (Surjection. metadata (empty active) (empty mirror)))
-    (equiv [this o] (or (and (isa? (class o) Surjection)
-                             (= active (.active ^Surjection o)))
-                        (= active o)))
-  Seqable (seq [this] (seq active))
   ILookup
     (valAt [this k]           (get active k))
     (valAt [this k not-found] (get active k not-found))
-  MapEquivalence
-  IPersistentSet
-    (disjoin [this [k v]]
-             (if (not (and (contains? active k) (contains? mirror v)))
-                 this
-                 (Surjection. metadata
-                             (dissoc active k)
-                             (disj mirror [v k]))))
-  IFn   (invoke [this k] (get active k))
-  IObj  (withMeta [this new-meta] (Surjection. new-meta active mirror))
-  IMeta (meta [this] metadata))
+  Seqable (seq    [this]   (seq active))
+  IFn     (invoke [this k] (get active k))
+  IMeta   (meta   [this]   metadata)
+  MapEquivalence)
 
 ; Dual of Surjection. Behaves like a SetMap, except it preserves the surjective property of the original map. Yields a Surjection when inverted.
 (deftype- InvertedSurjection [metadata
@@ -191,39 +186,36 @@
                              ^IPersistentMap mirror]
   Invertible (inverse [this] (surjection- metadata mirror active))
   IPersistentMap
-    (assoc [this k v]
-           (conj this [k v]))
-    (without [this k]
-             (reduce disj this (map (partial vector k) (get active k))))
+    (assoc   [this k v] (conj this [k v]))
+    (without [this k]   (reduce disj this (map (partial vector k) (get active k))))
+  IPersistentCollection
+    (cons [this [k v]]
+          (InvertedSurjection. metadata
+                               (assoc (disj active [(get mirror v) v]) k v)
+                               (assoc mirror v k)))
+    (equiv [this o]
+           (or (and (isa? (class o) InvertedSurjection)
+                    (= active (.active ^InvertedSurjection o)))
+               (= active o)))
+    (count [this] (count active))
+    (empty [this] (InvertedSurjection. metadata (empty active) (empty mirror)))
+  IPersistentSet
+    (disjoin [this [k v]]
+             (if (and (contains? active k) (contains? mirror v))
+                 (InvertedSurjection. metadata (disj active [k v]) (dissoc mirror v)))
+             this)
+  IObj (withMeta [this new-meta] (InvertedSurjection. new-meta active mirror))
+  ; Everything below here is the same for all mirrored maps.
   Associative
     (containsKey [this k] (contains? active k))
     (entryAt     [this k] (find active k))
-  IPersistentCollection
-    (count [this] (count active))
-    (cons [this [k v]]
-          (InvertedSurjection. metadata
-                               (assoc (dissoc active (get mirror v)) k v)
-                               (assoc mirror v k)))
-    (empty [this]
-           (InvertedSurjection. metadata (empty active) (empty mirror)))
-    (equiv [this o] (or (and (isa? (class o) InvertedSurjection)
-                             (= active (.active ^InvertedSurjection o)))
-                        (= active o)))
-  Seqable (seq [this] (seq active))
   ILookup
     (valAt [this k]           (get active k))
     (valAt [this k not-found] (get active k not-found))
-  MapEquivalence
-  IPersistentSet
-    (disjoin [this [k v]]
-             (if (not (and (contains? active k) (contains? mirror v)))
-                 this
-                 (InvertedSurjection. metadata
-                                      (disj active [k v])
-                                      (dissoc mirror v))))
-  IFn  (invoke [this k] (get active k))
-  IObj (withMeta [this new-meta] (InvertedSurjection. new-meta active mirror))
-  IMeta (meta [this] metadata))
+  Seqable (seq    [this]   (seq active))
+  IFn     (invoke [this k] (get active k))
+  IMeta   (meta   [this]   metadata)
+  MapEquivalence)
 
 ; Private factory functions for InvertedSurjection and Surjection. Required because of limitations on coroutined type definitions.
 (defn- inverted-surjection-
