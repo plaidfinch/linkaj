6c581f6589e15d983b9ccb854be1466e7b13d3e3
Kenneth Foner <kenny.foner@gmail.com>
2013-05-22 18:01:23 -0400

    Added new quasi-macro -#- which skips threading insertion for the form directly inside it, but resumes inside each of those.

--- src/map_experiments/directed_graph/macro.clj
+++ src/map_experiments/directed_graph/macro.clj
@@ -7,22 +7,30 @@
   (fn [form symb] (class form)))
 
 ; This is used to stop graph threading inside its application
-(declare -#|)
+(declare -#| -#-)
 (def graph-stop-threading-symb
   (symbol "-#|"))
+(def graph-skip-threading-symb
+  (symbol "-#-"))
 
 ; Any list beginning with a symbol that resolves to something in the core or protocol namespace is prefixed with the threaded symbol.
 (defmethod graph-thread-insert clojure.lang.PersistentList [form symb]
   (with-meta
     (let [function (first form)
-          rest-form (map #(graph-thread-insert % symb) (rest form))]
+          rest-form (map #(graph-thread-insert % symb) (rest form))
+          rest-form-skip (map (fn [x] (map #(graph-thread-insert % symb) x))
+                              (rest form))
+          rest-form-stop (rest form)]
          (if (symbol? function)
-             (if (= (resolve function) (resolve graph-stop-threading-symb))
-                 `(do ~@(rest form))
-                 (cons (first form)
-                       (cons symb
-                             (map #(graph-thread-insert % symb)
-                                  (rest form)))))
+             (cond (= (resolve function) (resolve graph-stop-threading-symb))
+                   `(do ~@rest-form-stop)
+                   (= (resolve function) (resolve graph-skip-threading-symb))
+                   `(do ~@rest-form-skip)
+                   :else
+                   (cons (first form)
+                         (cons symb
+                               (map #(graph-thread-insert % symb)
+                                    (rest form)))))
              (cons (graph-thread-insert function symb) rest-form)))
     (meta form)))
 
