732164e9d81860eb8715bbf86aa47d553705bdbd
Kenneth Foner <kenny.foner@gmail.com>
2013-08-09 13:41:03 -0400

    Added description and real version number to project.clj.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject map-experiments "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject map-experiments "0.5.0-SNAPSHOT"
+  :description "Persistent associative datatypes with more features than Clojure's built-in maps, including bijective maps, key-attribute-value maps, bipartite graphs, and directed graphs with arbitrary node and edge attributes."
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :global-vars {*print-length* 500})
