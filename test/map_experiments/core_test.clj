1076ea8bf7e93a3d10e2a6665dacfeb82263941d
Kenneth Foner <kenny.foner@gmail.com>
2013-09-08 14:04:53 -0400

    Removed dependency on (refresh) and also added correct imports to unused-as-now test suite.

--- test/map_experiments/core_test.clj
+++ test/map_experiments/core_test.clj
@@ -1,6 +1,13 @@
 (ns map-experiments.core-test
   (:require [clojure.test :refer :all]
-            [map-experiments.core :refer :all]))
+            [map-experiments.common                  :refer :all]
+            [map-experiments.smart-maps.protocol     :refer :all]
+            [map-experiments.smart-maps.bijection    :refer :all]
+            [map-experiments.smart-maps.surjection   :refer :all]
+            [map-experiments.smart-maps.bipartite    :refer :all]
+            [map-experiments.smart-maps.attr-map     :refer :all]
+            [map-experiments.directed-graph.protocol :refer :all]
+            [map-experiments.directed-graph.macro    :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
